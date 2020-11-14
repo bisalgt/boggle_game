ed09caddb8a982ea091cfb9706173493ac1e3f5a
Bishal Gautam <bisalgt@gmail.com>
2020-11-15 00:41:37 +0545

    timer of 30 sec added, integrating with our function remaning

--- boogle_project/static/js/usingAjax.js
+++ boogle_project/static/js/usingAjax.js
@@ -1,4 +1,5 @@
 $("document").ready(function () {
+  var numbers = [1, 2, 3];
   $("#butn").click(function () {
     console.log("button clicked");
     $.ajax({
@@ -7,8 +8,29 @@ $("document").ready(function () {
       data: { name: "bishal" },
       success: function (response) {
         console.log("successful");
-        console.log(response);
+        numbers.push(1);
+        console.log(numbers);
+        var para = document.createElement("p");
+        var paraText = document.createTextNode("Response" + numbers);
+        para.appendChild(paraText);
+        var element = document.getElementById("div1");
+        console.log(element, " this is the element div");
+        element.appendChild(para);
+      },
+      error: function (error) {
+        console.log(error);
       },
     });
   });
+  var total_time = 30;
+  var difference = 30;
+  function countDownTimer() {
+    console.log("Difference time", difference);
+
+    difference -= 1;
+    console.log("Difference-----time", difference);
+    var para = document.getElementById("timerId");
+    para.innerHTML = difference;
+  }
+  setInterval(countDownTimer, 1000);
 });
